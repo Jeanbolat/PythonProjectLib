f0a76605ee1a75afc022986af8a9bf99c2a772c2
Kagan <kagantegin@gmail.com>
2020-08-05 19:54:11 +0600

    change readme file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name="newModels",
+    url="https://github.com/Jeanbolat/PythonProjectLib",
+    author="kirigaikabuto",
+    packages=["models"],
+    install_requires=[],
+    license="MIT",
+    description="Library for me",)
